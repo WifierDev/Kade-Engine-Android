b2a52e0093c4670819c8bbf891e168333bd3aa84
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-02 11:44:02 +0200

    ||||

--- source/NoteskinHelpers.hx
+++ source/NoteskinHelpers.hx
@@ -1,3 +1,7 @@
+#if FEATURE_FILESYSTEM
+import sys.FileSystem;
+import sys.io.File;
+#end
 import openfl.display.BitmapData;
 import flixel.FlxSprite;
 import flixel.graphics.FlxGraphic;
@@ -15,7 +19,26 @@ class NoteskinHelpers
 	{
 		noteskinArray = [];
 		xmlData = [];
+		#if FEATURE_FILESYSTEM
+		var count:Int = 0;
+		for (i in FileSystem.readDirectory(FileSystem.absolutePath(SUtil.getPath() + "assets/shared/images/noteskins")))
+		{
+			if (i.contains("-pixel"))
+				continue;
+			if (i.endsWith(".xml"))
+			{
+				xmlData.push(sys.io.File.getContent(FileSystem.absolutePath(SUtil.getPath() + "assets/shared/images/noteskins") + "/" + i));
+				continue;
+			}
+
+			if (!i.endsWith(".png"))
+				continue;
+			noteskinArray.push(i.replace(".png", ""));
+		}
+		#else
 		noteskinArray = ["Arrows", "Circles"];
+		#end
+
 		return noteskinArray;
 	}
 
@@ -31,11 +54,40 @@ class NoteskinHelpers
 
 	static public function generateNoteskinSprite(id:Int)
 	{
+		#if FEATURE_FILESYSTEM
+		// TODO: Make this use OpenFlAssets.
+
+		Debug.logTrace("bruh momento");
+
+		var path = FileSystem.absolutePath(SUtil.getPath() + "assets/shared/images/noteskins") + "/" + getNoteskinByID(id);
+		var data:BitmapData = BitmapData.fromFile(path + ".png");
+
+		return FlxAtlasFrames.fromSparrow(FlxGraphic.fromBitmapData(data), xmlData[id]);
+
+		// return Paths.getSparrowAtlas('noteskins/' + NoteskinHelpers.getNoteskinByID(FlxG.save.data.noteskin), "shared");
+		#else
 		return Paths.getSparrowAtlas('noteskins/Arrows', "shared");
+		#end
 	}
 
 	static public function generatePixelSprite(id:Int, ends:Bool = false)
 	{
-		return BitmapData.fromFile(SUtil.getPath() + Paths.image('noteskins/Arrows-pixel', "shared"));
+		#if FEATURE_FILESYSTEM
+		// TODO: Make this use OpenFlAssets.
+
+		Debug.logTrace("bruh momento");
+
+		var path = FileSystem.absolutePath(SUtil.getPath() + "assets/shared/images/noteskins") + "/" + getNoteskinByID(id) + "-pixel" + (ends ? "-ends" : "");
+		if (!FileSystem.exists(path + ".png"))
+		{
+			Debug.logTrace("getting default pixel skin");
+			return BitmapData.fromFile(FileSystem.absolutePath(SUtil.getPath() + "assets/shared/images/noteskins") + "/Arrows-pixel" + (ends ? "-ends" : "") + ".png");
+		}
+		return BitmapData.fromFile(path + ".png");
+
+		// return Paths.getSparrowAtlas('noteskins/' + NoteskinHelpers.getNoteskinByID(FlxG.save.data.noteskin), "shared");
+		#else
+		return BitmapData.fromFile(Paths.image('noteskins/Arrows-pixel', "shared"));
+		#end
 	}
 }
